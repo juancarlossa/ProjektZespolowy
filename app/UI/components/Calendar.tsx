3484b76ad45a99f9695f913916c57468cf376b99
juancarlossa <juank987610@gmail.com>
2024-05-24 18:54:39 +0200

    Added Calendar and RangeCalendar components

---
+++ app/UI/components/Calendar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Calendar } from "@nextui-org/react";
+import { today, getLocalTimeZone, isWeekend } from "@internationalized/date";
+import { useLocale } from "@react-aria/i18n";
+
+
+export default function App () {
+  let [date, setDate] = React.useState(today(getLocalTimeZone()));
+  let { locale } = useLocale();
+  let isInvalid = isWeekend(date, locale);
+
+  return (
+    <Calendar
+      aria-label="Date (Invalid on weekends)"
+      errorMessage={isInvalid ? "We are closed on weekends" : undefined}
+      isInvalid={isInvalid}
+      value={date}
+      onChange={setDate}
+      showMonthAndYearPickers
+      defaultValue={today(getLocalTimeZone())}
+      minValue={today(getLocalTimeZone())}
+    />
+  );
+}
