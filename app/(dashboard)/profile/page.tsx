adb200937f0328b3313098ea9c13ec389390a2ed
juancarlossa <juank987610@gmail.com>
2024-06-25 23:08:53 +0200

    Poprawiona strona 'Profile'

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -8,6 +8,7 @@ import toast, { Toaster } from "react-hot-toast";
 import { redirect } from "next/navigation";
 import Nav from "../../UI/components/Navbar";
 import { Avatar } from "@nextui-org/react";
+import InputText from "@/app/UI/components/Input";
 
 export default async function Profile () {
   const supabase = createServerComponentClient({ cookies })
@@ -19,15 +20,21 @@ export default async function Profile () {
   }
   let friendList = await getFriendsByUserId(user.id)
   if (friendList === undefined) return ''
+
   return (
     <>
       <Nav userImg={user.user_metadata.avatar_url} />
-      <div className="flex w-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
-        <h1>Email: {user.email}</h1>
-        <h1>Username: {user.email!.split('@')[0]}</h1>
-        <div className="flex flex-row justify-between w-40">
-          <h2>Profile picture: </h2>
-          <Avatar src={user.user_metadata.avatar_url} alt="" className='rounded-full' isBordered size='md' />
+      <div className="flex flex-col items-center w-full py-12 sm:px-6 lg:px-8 bg-gray-100">
+        <div className="bg-white shadow sm:rounded-lg p-6 w-full max-w-md">
+          <div className="flex flex-row">
+            <InputText
+              email={user.email!} username={user.email!.split('@')[0]}
+            />
+          </div>
+          <div className="mt-4 flex items-center">
+            <h2 className="text-lg font-medium text-gray-900">Profile picture: </h2>
+            <Avatar src={user.user_metadata.avatar_url} alt="" className="ml-4 rounded-full" isBordered size="md" />
+          </div>
         </div>
       </div>
     </>
