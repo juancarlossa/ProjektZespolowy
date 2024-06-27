ebcd7d3e5d0e2b50b09716b59beea5280627680b
juancarlossa <juank987610@gmail.com>
2024-06-27 20:05:40 +0200

    Funkcja update name + component

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -9,6 +9,10 @@ import { redirect } from "next/navigation";
 import Nav from "../../UI/components/Navbar";
 import { Avatar } from "@nextui-org/react";
 import InputText from "@/app/UI/components/Input";
+import { getName } from "@/app/_backend/actions/update-name";
+import { getSurname } from "@/app/_backend/actions/update-surname";
+import { getTelefon } from "@/app/_backend/actions/update-telefon";
+import { getPicture } from "@/app/_backend/actions/update-picture";
 
 export default async function Profile () {
   const supabase = createServerComponentClient({ cookies })
@@ -21,20 +25,37 @@ export default async function Profile () {
   let friendList = await getFriendsByUserId(user.id)
   if (friendList === undefined) return ''
 
+  const uname = await getName()
+  const surname = await getSurname()
+  const telefon = await getTelefon()
+  const picture = await getPicture()
+  const joiningDate = new Date(user.created_at).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
   return (
     <>
       <Nav userImg={user.user_metadata.avatar_url} />
       <div className="flex flex-col items-center w-full py-12 sm:px-6 lg:px-8 bg-gray-100">
+        <h1 className="my-5 font-bold text-3xl text-slate-600">Update your profile</h1>
         <div className="bg-white shadow sm:rounded-lg p-6 w-full max-w-md">
-          <div className="flex flex-row">
+          <div className="flex flex-col">
             <InputText
-              email={user.email!} username={user.email!.split('@')[0]}
+              email={user.email!}
+              username={user.email!.split('@')[0]}
+              telefon={telefon}
+              uname={uname}
+              surname={surname}
+              picture={picture}
             />
           </div>
           <div className="mt-4 flex items-center">
             <h2 className="text-lg font-medium text-gray-900">Profile picture: </h2>
             <Avatar src={user.user_metadata.avatar_url} alt="" className="ml-4 rounded-full" isBordered size="md" />
           </div>
+          <h1 className="mt-8 font-light font italic">Created at: {joiningDate}</h1>
         </div>
       </div>
     </>
--- app/UI/components/Input.tsx
+++ app/UI/components/Input.tsx
@@ -2,17 +2,26 @@
 import React, { useRef } from "react";
 import { Button, Input } from "@nextui-org/react";
 import { updateEmail, updateUsername } from "@/app/_backend/actions/update-email";
+import { updateTelefon } from "@/app/_backend/actions/update-telefon";
+import { updateName } from "@/app/_backend/actions/update-name";
+import { updateSurname } from "@/app/_backend/actions/update-surname";
+import { updatePicture } from "@/app/_backend/actions/update-picture";
 
 type Props = {
   email: string
   username: string
+  telefon: string
+  uname: string
+  surname: string
+  picture: string
 }
 
-export default function InputText ({ email, username }: Props) {
+export default function InputText ({ email, username, telefon, uname, surname, picture }: Props) {
   const variants = ["flat", "bordered", "underlined", "faded"];
   const formRef = useRef<HTMLFormElement>(null)
-  return (
-    <section>
+
+  const InputEmail = () => {
+    return (
       <form
         ref={formRef}
         action={async (formData) => {
@@ -39,7 +48,11 @@ export default function InputText ({ email, username }: Props) {
           </div>
         </div>
       </form>
+    )
+  }
 
+  const InputUsername = () => {
+    return (
       <form
         ref={formRef}
         action={async (formData) => {
@@ -64,6 +77,133 @@ export default function InputText ({ email, username }: Props) {
           </div>
         </div>
       </form>
+    )
+  }
+
+  const InputTelefon = () => {
+    return (
+      <form
+        ref={formRef}
+        action={async (formData) => {
+          await updateTelefon(formData)
+          formRef.current?.reset()
+        }}
+        className='flex-1 border-t border-gray-300 px-4 py-4 mb-2 sm:mb-0'
+      >
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex w-full flex-row md:flex-nowrap mb-6 md:mb-0 gap-4">
+            <h1 className="mt-2 text-lg font-medium text-gray-900">Telefon: </h1>
+            <Input defaultValue={telefon} name='telefon' variant="underlined" placeholder="Enter your telefon" />
+            <Button
+              type='submit'
+              color='primary'
+              className='rounded-full'
+              size='md'
+              variant='ghost'
+            >
+              Post
+            </Button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+
+  const InputName = () => {
+    return (
+      <form
+        ref={formRef}
+        action={async (formData) => {
+          await updateName(formData)
+          formRef.current?.reset()
+        }}
+        className='flex-1 border-t border-gray-300 px-4 py-4 mb-2 sm:mb-0'
+      >
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex w-full flex-row md:flex-nowrap mb-6 md:mb-0 gap-4">
+            <h1 className="mt-2 text-lg font-medium text-gray-900">Name: </h1>
+            <Input defaultValue={uname} name='name' variant="underlined" placeholder="Enter your name" />
+            <Button
+              type='submit'
+              color='primary'
+              className='rounded-full'
+              size='md'
+              variant='ghost'
+            >
+              Post
+            </Button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+
+  const InputSurname = () => {
+    return (
+      <form
+        ref={formRef}
+        action={async (formData) => {
+          await updateSurname(formData)
+          formRef.current?.reset()
+        }}
+        className='flex-1 border-t border-gray-300 px-4 py-4 mb-2 sm:mb-0'
+      >
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex w-full flex-row md:flex-nowrap mb-6 md:mb-0 gap-4">
+            <h1 className="mt-2 text-lg font-medium text-gray-900">Surname: </h1>
+            <Input defaultValue={surname} name='surname' variant="underlined" placeholder="Enter your surname" />
+            <Button
+              type='submit'
+              color='primary'
+              className='rounded-full'
+              size='md'
+              variant='ghost'
+            >
+              Post
+            </Button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+
+  const InputPicture = () => {
+    return (
+      <form
+        ref={formRef}
+        action={async (formData) => {
+          await updatePicture(formData)
+          formRef.current?.reset()
+        }}
+        className='flex-1 border-t border-gray-300 px-4 py-4 mb-2 sm:mb-0'
+      >
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex w-full flex-row md:flex-nowrap mb-6 md:mb-0 gap-4">
+            <h1 className="mt-2 text-lg font-medium text-gray-900">Picture: </h1>
+            <Input defaultValue={picture} name='picture' variant="underlined" placeholder="Enter your surname" />
+            <Button
+              type='submit'
+              color='primary'
+              className='rounded-full'
+              size='md'
+              variant='ghost'
+            >
+              Post
+            </Button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+
+  return (
+    <section>
+      <InputEmail />
+      <InputUsername />
+      <InputTelefon />
+      <InputName />
+      <InputSurname />
+      <InputPicture />
     </section>
   );
 }
